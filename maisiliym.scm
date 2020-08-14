f5d026a3ad8f9513e13d116ceef473ea9a80d2be
li <@li:maisiliym.xyz>
2020-08-14 04:13:47 +0000

    (rebase publish)

---
+++ maisiliym.scm
@@ -0,0 +1,7 @@
+(define-module (maisiliym)
+  #:use-module (guix gexp)
+  #:export (li-ssh-priKriomz))
+
+(define li-ssh-priKriomz
+  (list (local-file "priKriomz/li-xerxes.pub")
+	(local-file "priKriomz/li-dante.pub")))
