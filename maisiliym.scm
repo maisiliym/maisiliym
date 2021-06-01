6828edbcac8de6e2909fe34f03f1d8ccbc15f46d
li <@li:maisiliym.uniks>
2021-06-01 16:10:04 +0700

    (snap cleanupHack)

--- maisiliym.scm
+++ maisiliym.scm
@@ -2,20 +2,10 @@
   #:use-module (guix gexp)
   #:export (root-li-authorized-keys))
 
-(define li-ssh-prikriomz
-  (list (local-file "priKriomz/li-xerxes.pub")
-	(local-file "priKriomz/li-dante.pub")))
-
-(define (make-authorized-key user prikriom)
-  (list user prikriom))
-
-(define (make-li-key prikriom)
-  (make-authorized-key "li" prikriom))
-
-(define (make-root-key prikriom)
-  (make-authorized-key "root" prikriom))
+(define li-ssh-prikriomz-file
+  (local-file "priKriomz/li-dante.pub"))
 
 (define root-li-authorized-keys
-  (append
-   (map make-li-key li-ssh-prikriomz)
-   (map make-root-key li-ssh-prikriomz)))
+  (list
+   (list "root" li-ssh-prikriomz-file)
+   (list "li" li-ssh-prikriomz-file)))
