2be4383fd69008fe2b380fb17e0b0f12d7c0454c
li <@li:maisiliym.uniks>
2021-06-01 12:32:35 +0700

    (snap workingLiRootKeys)

--- maisiliym.scm
+++ maisiliym.scm
@@ -1,7 +1,21 @@
 (define-module (maisiliym)
   #:use-module (guix gexp)
-  #:export (li-ssh-priKriomz))
+  #:export (root-li-authorized-keys))
 
-(define li-ssh-priKriomz
+(define li-ssh-prikriomz
   (list (local-file "priKriomz/li-xerxes.pub")
 	(local-file "priKriomz/li-dante.pub")))
+
+(define (make-authorized-key user prikriom)
+  (list user prikriom))
+
+(define (make-li-key prikriom)
+  (make-authorized-key "li" prikriom))
+
+(define (make-root-key prikriom)
+  (make-authorized-key "root" prikriom))
+
+(define root-li-authorized-keys
+  (append
+   (map make-li-key li-ssh-prikriomz)
+   (map make-root-key li-ssh-prikriomz)))
