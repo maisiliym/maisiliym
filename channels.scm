6664897f6942501e495a906abc8618fd3a78c47d
li <@li:maisiliym.uniks>
2021-06-05 18:38:35 +0700

    (snap initChannels)

---
+++ channels.scm
@@ -0,0 +1,23 @@
+(list
+ (channel
+  (name 'guix-home-manager)
+  (url "https://framagit.org/tyreunom/guix-home-manager.git")
+  (branch "master")
+  (commit
+   "7567a9b1dd5606de5dd105ba6b8bf2591017eb64")
+  (introduction
+   (make-channel-introduction
+    "b5f32950a8fa9c05efb27a0014f9b336bb318d69"
+    (openpgp-fingerprint
+     "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82"))))
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (branch "master")
+  (commit
+   "516e9dda6b789c8a07e96dda26585702f79e891b")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
