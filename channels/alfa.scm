fa685188ee8fb768a75b637b904756ccdfc745a9
li <li@maisiliym.uniks>
2021-06-28 16:58:14 +0700

    (snap alfaChannel)

---
+++ channels/alfa.scm
@@ -0,0 +1,23 @@
+(list
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (branch "master")
+  (commit
+   "516e9dda6b789c8a07e96dda26585702f79e891b")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+  (name 'uniks)
+  (url "https://github.com/maisiliym/uniks")
+  (branch "skiim")
+  (commit
+   "ae73ab6ce4978fb28e25f7ff473f0c51e430e075")
+  (introduction
+   (make-channel-introduction
+    "ae73ab6ce4978fb28e25f7ff473f0c51e430e075"
+    (openpgp-fingerprint
+     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223")))))
