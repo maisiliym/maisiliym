d9a867d723c8cf71d7d9fae46ec587229b23bd3d
li <li@maisiliym.uniks>
2021-07-02 13:44:42 +0700

    (snap priAlfaChannel)

---
+++ channels/pri-alfa.scm
@@ -0,0 +1,53 @@
+(list
+ (channel
+  (name 'uniks)
+  (url "https://github.com/maisiliym/uniks")
+  (branch "skiim")
+  (commit
+   "9e9102ca244ce6c85a652a2a405d50de42230c91")
+  (introduction
+   (make-channel-introduction
+    "9e9102ca244ce6c85a652a2a405d50de42230c91"
+    (openpgp-fingerprint
+     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223"))))
+ (channel
+  (name 'giiks)
+  (url "https://github.com/maisiliym/giiks")
+  (branch "pri-alfa")
+  (introduction
+   (make-channel-introduction
+    "4bcb8178ffc623c11934b9d48e797ece16bc0818"
+    (openpgp-fingerprint
+     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223"))))
+ (channel
+  (name 'guix-home-manager)
+  (url "https://framagit.org/tyreunom/guix-home-manager.git")
+  (branch "master")
+  (commit
+   "7567a9b1dd5606de5dd105ba6b8bf2591017eb64")
+  (introduction
+   (make-channel-introduction
+    "b5f32950a8fa9c05efb27a0014f9b336bb318d69"
+    (openpgp-fingerprint
+     "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82"))))
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (branch "master")
+  (commit
+   "516e9dda6b789c8a07e96dda26585702f79e891b")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  (commit
+   "935a9e4f124589e9c4989497d9d4f0d3e5f4c2f4")
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
