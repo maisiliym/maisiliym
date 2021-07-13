bab4eb88e065147a1cb3c84dba4c47f4733b9213
li <li@maisiliym.uniks>
2021-07-13 07:50:34 +0700

    (snap workingChannels)

--- channels/alfa.scm
+++ channels/alfa.scm
@@ -1,23 +1,64 @@
 (list
  (channel
-  (name 'guix)
-  (url "https://git.savannah.gnu.org/git/guix.git")
-  (branch "master")
-  (commit
-   "516e9dda6b789c8a07e96dda26585702f79e891b")
+  (name 'raizyn)
+  (url "https://github.com/maisiliym/maisiliym")
+  (branch "mein")
   (introduction
    (make-channel-introduction
-    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    "10362ca5c2044f91a4626948fd3a85b0ea6327e5"
     (openpgp-fingerprint
-     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223"))))
  (channel
   (name 'uniks)
   (url "https://github.com/maisiliym/uniks")
   (branch "skiim")
-  (commit
-   "9e9102ca244ce6c85a652a2a405d50de42230c91")
   (introduction
    (make-channel-introduction
     "9e9102ca244ce6c85a652a2a405d50de42230c91"
     (openpgp-fingerprint
-     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223")))))
+     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223"))))
+ (channel
+  (name 'flat)
+  (url "https://github.com/flatwhatson/guix-channel.git")
+  (commit "91fd421b488aabeb4eb4e17aacdee395212d93b4")
+  (introduction
+   (make-channel-introduction
+    "33f86a4b48205c0dc19d7c036c85393f0766f806"
+    (openpgp-fingerprint
+     "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+ (channel
+  (name 'giiks)
+  (url "https://github.com/maisiliym/giiks")
+  (branch "alfa")
+  (introduction
+   (make-channel-introduction
+    "e0d45d835762afd3570badce9cd07fcd57f835ac"
+    (openpgp-fingerprint
+     "775A 7994 B5A7 0AD5 2D7E  B03B EE38 5D17 F4AD 0223"))))
+ (channel
+  (name 'guix-home-manager)
+  (url "https://framagit.org/tyreunom/guix-home-manager.git")
+  (branch "master")
+  (introduction
+   (make-channel-introduction
+    "b5f32950a8fa9c05efb27a0014f9b336bb318d69"
+    (openpgp-fingerprint
+     "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82"))))
+ (channel
+  (name 'guix)
+  (url "file:///git/git.savannah.gnu.org/git/guix")
+  (commit "94f772dad8cb1064d0e8cf11285ad9963ba00224")
+  (introduction
+   (make-channel-introduction
+    "94f772dad8cb1064d0e8cf11285ad9963ba00224"
+    (openpgp-fingerprint
+     "A28BF40C3E551372662D14F741AAE7DCCA3D8351"))))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  (commit "935a9e4f124589e9c4989497d9d4f0d3e5f4c2f4")
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
