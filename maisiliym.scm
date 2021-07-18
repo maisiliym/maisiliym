eb021bf2b52131615f9af1e08e2021061ab8f285
li <li@maisiliym.uniks>
2021-07-18 12:06:39 +0700

    (snap preNeimChange)

--- maisiliym.scm
+++ maisiliym.scm
@@ -1,62 +1,59 @@
-(define-module (maisiliym))
+(define-module (kriyraizyn))
 (use-modules (oop goops)
 	     (guix gexp)
 	     (uniks))
-(export root-li-authorized-keys metaneksys)
+(export maisiliym-neksys)
 
-(define metaneksys
-  (make <metaneksys>
-    #:neim "maisiliym"
-    #:trost 2
-    #:neksiz neksiz))
-
-(define xerxes
-  (cons "xerxes"
-	(make <neksys>
-	  #:neim "xerxes"
-	  #:spici "haibrid"
-	  #:trost 2
-	  #:saiz 2
-	  #:prikriom xerxes-prikriom)))
-
-(define dante
-  (cons "dante"
-	(make <neksys>
-	  #:neim "dante"
-	  #:spici "haibrid"
-	  #:trost 2
-	  #:saiz 2
-	  #:prikriom dante-prikriom)))
-
-(define xerxes-prikriom
-  (make <prikriom>
-    #:ful ""
-    #:ssh ""
-    #:keygrip ""))
-
-(define dante-prikriom
-  (make <prikriom>
-    #:ful ""
-    #:ssh ""
-    #:keygrip ""))
-
-(define li-ssh-prikriomz-file
-  (local-file "prikriomz/li.pub"))
-
-(define root-li-authorized-keys
-  (list
-   (list "root" li-ssh-prikriomz-file)
-   (list "li" li-ssh-prikriomz-file)))
-
-(define-public %guix-authorized-keys
-  (list
-   (local-file "prikriomz/dante-guix.pub")
-   (local-file "prikriomz/xerxes-guix.pub")))
-
-(define-public %li-keygrips
-  (list
-   "AD305831DD33E62F9AD587718D4E5E6999CD84EA"
-   "63A149ECC539BAF2B001C57E9A55A566BCD48446"))
-
-(define-public %guix-substitute-urls
-  (list "http://[fd43:6908:a1dd:0:685c:4fef:f9bf:ed56]:8080")) ; verihaki
+(define maisiliym-kriyraizyn
+  (make <kriyraizyn>
+    #:neksiz
+    (list
+     (cons
+      "maisiliym"
+      (make <neksys>
+	#:neim "maisiliym"
+	#:trost 2
+	#:prineksiz
+	(list
+	 (cons
+	  "xerxes" (make <prineksys>
+		     #:neim "xerxes"
+		     #:spici "haibrid"
+		     #:trost 2
+		     #:saiz 2
+		     #:prikriom
+		     (make <prikriom>
+		       #:pidjipi ""
+		       #:ssh ""
+		       #:keygrip "")))
+	 (cons
+	  "dante" (make <prineksys>
+		    #:neim "dante"
+		    #:spici "haibrid"
+		    #:trost 2
+		    #:saiz 2
+		    #:prikriom
+		    (make <prikriom>
+		      #:pidjipi ""
+		      #:ssh ""
+		      #:keygrip ""))))
+	#:krimynz
+	(list
+	 (make <krimyn>
+	   #:neim "li"
+	   #:spici "kodyr"
+	   #:trost 2
+	   #:saiz 2
+	   #:github "maisiliym"
+	   #:prikriomz
+	   (list
+	    (cons
+	     "xerxes" (make <prikriom>
+			#:ful (get-string-all "./xerxes.asc")
+			#:ssh "AAAAC3NzaC1lZDI1NTE5AAAAIEygb1Ft1hIB+ExPGLq08im9rFYvOeYXX+NetgqjI3Db"
+			#:keygrip "AD305831DD33E62F9AD587718D4E5E6999CD84EA"))
+	    (cons
+	     "dante" (make <prikriom>
+		       #:ful (get-string-all "./dante.asc")
+		       #:ssh "AAAAC3NzaC1lZDI1NTE5AAAAIEygb1Ft1hIB+ExPGLq08im9rFYvOeYXX+NetgqjI3Db"
+		       #:keygrip "AD305831DD33E62F9AD587718D4E5E6999CD84EA"))))))))))
